290a38e17fd6e4d540b55f372c173e736747686e
Anmol Sharma <122697251+Anmol-Sharma21@users.noreply.github.com>
2023-11-14 03:40:16 +0530

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -2,18 +2,16 @@ import smtplib
 import datetime as dt
 import random
 
-my_email = "anmol.pythontest@gmail.com"
-my_password = "vyitymcetjrepnqz"
+my_email = "xyz@gmail.com" #ur email
+my_password = "xyz"        #ur app passowrd
 
 now = dt.datetime.now()
 weekday = now.weekday()
-if weekday == 1:
-    with open("quotes.txt") as quote_file:
+with open("quotes.txt") as quote_file:
         all_quotes = quote_file.readlines()
         quote = random.choice(all_quotes)
-    print(quote)
     with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
         connection.login(user=my_email, password=my_password)
-        connection.sendmail(from_addr=my_email, to_addrs="anmol.pythontest@yahoo.com",
+        connection.sendmail(from_addr=my_email, to_addrs="xyz@yahoo.com",       # to_addrs= senders emails 
                             msg=f"Subject: motivation \n\n{quote}")
